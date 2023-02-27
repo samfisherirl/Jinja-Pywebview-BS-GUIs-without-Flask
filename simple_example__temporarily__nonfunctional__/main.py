78db8c6fa53ceb739cba89c5bc96aa39728efc1a
Sasha Alexander <98753696+samfisherirl@users.noreply.github.com>
2023-02-26 23:44:05 -0600

    __fixing_simple___

--- simple_example__temporarily__nonfunctional__/main.py
+++ simple_example__temporarily__nonfunctional__/main.py
@@ -1,32 +1,43 @@
 from pathlib import Path
 from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
 import webview
-import templates.inject_js_css as inject_js_css
-from os import walk, getcwd
-import running_processes_and_paths as processes
+import templates.inject_js_css as inject_js_css 
 import atexit
 
-from os.path import join
+######################################### 
 
-
-#########################################
-#########################################
-
-from templates.settings import file_settings as file_settings
-
-#########################################
-
-# file_settings.py located in /templates/ folder
+from settings import file_settings
 
 Files = file_settings()
 
 #########################################
 # Inject CSS and JS files into the HTML
-inject_js_css.convert(Files)
-######################################### 
 
-###########running_processes############# 
-data = processes.get()
+inject_js_css.convert(Files)
+
+#########################################
+# Inject CSS and JS files into the HTML
+#########################################
+
+items = ["apple", "bannana", "peach"]
+
+class data:
+    def __init__(self, item):
+        self.item = item
+
+def construct_objs():
+    list_of_objs = []
+    for i in items:
+        list_of_objs.append(data(i))
+    return list_of_objs
+
+
+#########################################
+
+data = construct_objs()
+
+list = items
+
 ######################################### 
 
 # Create the environment
@@ -35,37 +46,32 @@ env = Environment(
     autoescape=select_autoescape()
 )
 
-
+#########################################
 
 # Get the template file
 template = env.get_template(Files.fname)
 
-# Create the list of words for the for loop
-words = ["this is a for loop", "this is a jinja for loop", "for loop 3"]
+#########################################
 
 
-
-
-# Render the HTML with the for loop and the CSS/JS files
 view = template.render(
         data = data,
-        words = words,
-        subtitle = "To render the variables, insert the placeholder between {{}}"
-        )
-
-# Print the rendered HTML
+        list = list,
+        title = "To render the variables, insert the placeholder between {{}}"
+        ) 
 
 
 # Exit handler to restore the original HTML file
+
 def exit_handler():
     inject_js_css.restore_backup()
+    
 
 def start_window():
     windowTitle = "My window"
     webview.create_window(windowTitle, html=view, width=1100, height=900, fullscreen=False)
     webview.start()
     atexit.register(exit_handler)
-
 
 
 def main():
